37f159d5665d547532ada0c805287b7746910074
Debesh Jha <36955587+DebeshJha@users.noreply.github.com>
2020-01-13 19:11:52 +0100

    Delete diagram.py

--- diagram.py
+++
@@ -1,9 +0,0 @@
-
-from unet import Unet
-from resunet import ResUnet
-from m_resunet import ResUnetPlusPlus
-
-if __name__ == "__main__":
-    arch = ResUnetPlusPlus(input_size=256)
-    model = arch.build_model()
-    model.summary()
